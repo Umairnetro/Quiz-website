2fc591df8a83cdcd2bdc8cd6b7d42845b14b7e72
Umair Ahmed <umairjdu@gmail.com>
2024-07-22 01:57:09 +0500

    update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const joinBtn = document.querySelector("#join-btn");
 
 if (joinBtn) {
   joinBtn.addEventListener("click", () => {
-    window.location.href = "register.html";
+    window.location.href = "./other-pages/register.html";
   });
 }
 
@@ -49,7 +49,7 @@ if (loginBtn) {
       userLogin.password === user.password
     ) {
       sessionStorage.setItem("username", JSON.stringify(user));
-      window.location.href = "index.html";
+      window.location.href = "../index.html";
     } else {
       alert("Invalid username or password");
     }
@@ -57,11 +57,55 @@ if (loginBtn) {
 }
 // trigger
 const startBtn = document.querySelector("#start-btn");
-console.log(startBtn);
-if (localStorage.getItem("username")) {
-  const userdisplay = JSON.parse(localStorage.getItem("username"));
-  document.querySelector(".name").innerHTML =
-    "Welcome, " + userdisplay.username;
-  joinBtn.classList.add("active");
-  startBtn.classList.remove("active");
+if (startBtn) {
+  if (localStorage.getItem("username")) {
+    const userdisplay = JSON.parse(localStorage.getItem("username"));
+    document.querySelector(".name").innerHTML =
+      "Welcome, " + userdisplay.username;
+    joinBtn.classList.add("active");
+    startBtn.classList.remove("active");
+  }
+
+  startBtn.addEventListener("click", () => {
+    window.location.href = "./other-pages/quiz.html";
+  });
 }
+
+// Quiz
+const questions = [
+  {
+    question: "What is the capital of France?",
+    answers: [
+      { text: "Paris", correct: true },
+      { text: "London", correct: false },
+      { text: "Rome", correct: false },
+      { text: "Berlin", correct: false },
+    ],
+  },
+  // Add more questions as needed
+];
+
+const questionElement = document.querySelector("#question");
+const options = document.querySelector("ul");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+console.log(questionElement, options);
+
+const showQuestion = (question) => {
+  questionElement.innerHTML = question.question;
+  question.answers.forEach((opt) => {
+    let button = document.createElement("button");
+    button.classList.add("options");
+    button.innerHTML = opt.text;
+    button.addEventListener("click", AnswerSelected);
+    options.appendChild(button);
+  });
+};
+
+const AnswerSelected = (ans) => {
+  console.log("me");
+};
+
+showQuestion(questions[0]);
